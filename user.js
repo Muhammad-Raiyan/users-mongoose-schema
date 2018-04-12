c481f29e97d7ab815502f83d074ce2612ca87728
Raiyan <mislam0525@gmail.com>
2018-04-12 13:26:35 -0400

    Initial commit for users-mongoose-schema'

---
+++ user.js
@@ -0,0 +1,51 @@
+const mongoose  = require('mongoose');
+const Schema    = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+const timestamps = require('mongoose-timestamp');
+
+const userSchema = new Schema({
+    email: String,
+    username: String,
+    password: String,
+    confirmed: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    temporaryToken: {
+        type: String,
+        required: true
+    },
+    following: [{
+        username: String,
+        id: String
+    }],
+    followers: [{
+        username: String,
+        id: String
+    }]
+});
+
+userSchema.plugin(timestamps);
+const User = mongoose.model('user', userSchema);
+module.exports = User;
+
+
+// TODO Export these into a "hash.js"
+module.exports.hashPassword = async (password) => {
+    try{
+        const salt = await bcrypt.genSalt(10);
+        return await bcrypt.hash(password, salt);
+
+    } catch(error) {
+        throw new Error('Hashing failed: ' + error);
+    }
+};
+
+module.exports.comparePasswords = async (inputPassword, hashedPassword) => {
+    try {
+        return await bcrypt.compare(inputPassword, hashedPassword);
+    } catch (error) {
+        throw new Error('Comparing failed: ' + error);
+    }
+};
